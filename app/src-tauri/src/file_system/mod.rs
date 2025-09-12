b711e9f70a2c6641154519342b6f9686ef6fb4dd
Emily Lucia Antosch <emilyluciaantosch@web.de>
2025-09-12 18:52:34 +0200

    Ad

--- app/src-tauri/src/file_system/mod.rs
+++ app/src-tauri/src/file_system/mod.rs
@@ -1,11 +1,13 @@
 pub mod file_id;
 pub mod hash;
 pub mod scanner;
+pub mod thumbnails;
 pub mod utils;
 pub mod watcher;
 
 pub use file_id::*;
 pub use hash::*;
 pub use scanner::*;
+pub use thumbnails::*;
 pub use utils::*;
 pub use watcher::*;
