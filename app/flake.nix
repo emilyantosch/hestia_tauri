ac6b229b4c71446a43b52fd3c00c554269bb10a7
Emily Lucia Antosch <emilylucia.antosch@moia.io>
2025-12-29 16:57:42 +0100

    Add(flake.nix): DevShell

---
+++ app/flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Hestia Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            name = "hestia";
+            buildInputs = [
+              (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
+              pkg-config
+              at-spi2-atk
+              atkmm
+              cairo
+              gdk-pixbuf
+              glib
+              gtk3
+              harfbuzz
+              librsvg
+              libsoup_3
+              pango
+              webkitgtk_4_1
+              openssl
+            ];
+            NIX_SHELL = "hestia";
+            shellHook = "nu";
+          };
+      }
+    );
+}
